80dcd2a09cc0693515ba57b6d2e8de4464dcf020
Justin San Juan <justin.sj@alumni.ust.hk>
2023-05-19 15:52:33 -0400

    WIP: refactor docker app

--- payroll-app/next.config.js
+++ payroll-app/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     reactStrictMode: true,
     serverRuntimeConfig: {
         dbConfig: {
-            host: 'localhost',
+            host: process.env.DB_IP,
             port: 3306,
             user: 'root',
             password: process.env.DB_ROOT_PASSWORD,
